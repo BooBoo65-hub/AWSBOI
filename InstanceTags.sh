a287b43c2ddcbcea071ea267d53f96aadea1ee91
BooBoo65-hub <57906431+BooBoo65-hub@users.noreply.github.com>
2020-04-01 20:46:30 +0100

    Update InstanceTags.sh

--- InstanceTags.sh
+++ InstanceTags.sh
@@ -1,8 +1,7 @@
-awslookup() {
-  cmd="aws --profile $1 ec2 describe-instances --filters \"Name=tag:Name,Values=$2\" --query 'Reservations[].Instances[].[InstanceId,PublicDnsName,PrivateIpAddress,State.Name,InstanceType,join(\`,\`,Tags[?Key==\`Name\`].Value)]' --output ${3:-table}"
-  if [ $# -eq 4 ]
-  then
-    echo "Running $cmd"
-  fi
-  eval $cmd
-}
+aws ec2 describe-tags --filters Name=key,Values=Stack
+
+aws ec2 describe-tags --filters Name=value,Values=BooBoo-Test-1
+
+aws ec2 describe-tags --filters Name=key,Values=Stack Name=value,Values=BooBoo-Test-1
+
+aws ec2 describe-tags --filters Name=value,Values=BooBoo-SG
